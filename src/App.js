c08614e4510ff814e38bdc864393340600320593
Marlen Kalandarov <kalandarov.marlen2016@gmail.com>
2022-05-25 23:36:02 +0600

    setup routing auth 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
-  const { isLogged, role } = useSelector((state)=>state.counter.login);
   return (
     <div className="App">
     </div>
