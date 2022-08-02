5aba8657f7f57748eb5b9f53f2e3d36bc07b67aa
Marlen Kalandarov <kalandarov.marlen2016@gmail.com>
2022-08-02 19:32:01 +0600

    clubdetail and dialogs changed

--- src/conteiner/Dialogs.js
+++ src/conteiner/Dialogs.js
@@ -3,24 +3,24 @@ import DialogItem from '../components/DialogItem/index.jsx';
 
 const getLobbyPhoto = (item) => item.lobby_photo;
 const getDialogOption = (item, me, option) => {
-    const partner = item.users_id.find((item)=>item.id !== me.id);
+    const partner = item.users_id.find((item) => item.id !== me.id);
     return partner[option] === '' ? null : partner[option];
 }
 
 const Dialogs = ({ dialogsList, me, handleDialogClick }) => {
-    return ( 
-        <div >
-            {dialogsList.map(item => (
-                <DialogItem
-                    key={item.lobby_info.lobby_id}
-                    photo={item.direct ? getDialogOption(item, me, 'photo') : getLobbyPhoto(item.lobby_info)}
-                    last_message={item.last_message.text}
-                    name={item.direct ? getDialogOption(item, me, 'name').split('/').join(' ') : item.lobby_info.lobby_name}
-                    id={item.lobby_info.lobby_id}
-                    dialogClick={handleDialogClick}
+    return ( <
+        div > {
+            dialogsList.map(item => ( <
+                DialogItem key = { item.lobby_info.lobby_id }
+                photo = { item.direct ? getDialogOption(item, me, 'photo') : getLobbyPhoto(item.lobby_info) }
+                last_message = { item.last_message.text }
+                name = { item.direct ? getDialogOption(item, me, 'name').split('/').join(' ') : item.lobby_info.lobby_name }
+                id = { item.lobby_info.lobby_id }
+                dialogClick = { handleDialogClick }
                 />
-            ))}
-        </div>
+            ))
+        } <
+        /div>
     )
 }
 
